2244dc89ee4a88fcea96f58ef740bae8894065c9
avutuan <business.tuanvu@gmail.com>
2024-02-02 11:41:20 -0600

    starting the problem with an object-oriented approach

--- day2/adv0.2.0.py
+++ day2/adv0.2.0.py
@@ -1,1 +1,30 @@
 #adv0.2.0.py
+def file_open(filename):
+    # Open the file
+    with open(filename) as file:
+        # Read the file
+        file_contents = file.readlines()
+        # Format the file
+        formatted_file_contents = []
+        for line in file_contents:
+            formatted_file_contents.append(line[:-1:])
+        # Close the file
+        file.close()
+        # Print the file
+        return formatted_file_contents
+
+class Round:
+    def __init__(self, red, blue, green):
+        self.red = red
+        self.blue = blue
+        self.green = green
+    
+    
+
+class Game:
+    def __init__(self, round1, round2, round3, number):
+        self.round1 = Round(round1)
+        self.round2 = Round(round2)
+        self.round3 = Round(round3)
+        self.number = number
+        
